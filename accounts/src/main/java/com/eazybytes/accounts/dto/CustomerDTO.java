28a895f3dfdc6ee4d1e475e3fa465ea8ed5a068d
Jiri Jasonek <jiri.jasonek@principal.cz>
2025-04-28 22:12:22 +0200

    Add DTOs

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDTO.java
@@ -0,0 +1,11 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
